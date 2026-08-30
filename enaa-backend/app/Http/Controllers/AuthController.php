3d0a8533ecfc538e7b37a73e1a6471844db58f71
abderrahim177 <abdorrahimakarchach@gmail.com>
2026-08-30 19:46:54 +0100

    add login part

---
+++ enaa-backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Auth\Events\Attempting;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function Login(LoginRequest $request){
+        $inputsValidate = $request->validated();
+
+        if(!Auth::attempt($inputsValidate)){
+            return response()->json([
+                'message' => 'votre information est incorrecte !'
+            ], 401);
+        }
+            $user = User::where('email' , $inputsValidate['email'])->firstOrFail();
+            $user->tokens()->delete();
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Login success',
+            'user' => $user,
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ], 200);
+        
+    }
+    public function Logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Logged out successfully!'
+        ], 200);
+    }
+}
--- enaa-backend/routes/api.php
+++ enaa-backend/routes/api.php
@@ -1,8 +1,38 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LeaveRequestController;
+use App\Http\Controllers\LeaveTypeController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/login' , [AuthController::class , 'Login']);
+
+
+Route::middleware(['auth:sanctum', 'role:Admin RH'])->group(function () {
+    // Route::post('/leave-types', [LeaveTypeController::class, 'store']);
+    // Route::get('/export-paie', [LeaveRequestController::class, 'exportPaie']);
+    Route::post('/Logout' , [AuthController::class , 'Logout']);
+});
+
+
+Route::middleware(['auth:sanctum', 'role:Manager'])->group(function () {
+    // Route::post('/leave-requests/{id}/validate-n1', [LeaveRequestController::class, 'validateN1']);
+    Route::post('/Logout' , [AuthController::class , 'Logout']);
+});
+
+
+Route::middleware(['auth:sanctum', 'role:Formateur'])->group(function () {
+    // Route::post('/leave-requests/submit', [LeaveRequestController::class, 'store']);
+    Route::post('/Logout' , [AuthController::class , 'Logout']);
+});
+
+
+Route::middleware(['auth:sanctum', 'role:Admin RH|Manager'])->group(function () {
+    // Route::get('/team-calendar', [LeaveRequestController::class, 'teamCalendar']);
+    Route::post('/Logout' , [AuthController::class , 'Logout']);
+});
